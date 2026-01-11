78ea27c78172b55dcc4a11023dc4c834356dfaa0
Sam Grouchnikov <112674910+sam-hoodie@users.noreply.github.com>
2026-01-10 20:46:32 -0500

    Updates

--- DataCleaning.py
+++ DataCleaning.py
@@ -3,9 +3,9 @@ import pandas as pd
 # Modify the script for study-specific alterations (filter my prompt, response length, etc.)
 
 # Loads all items in from the original datasets (files ending in _items_sctt)
-dfTrain = pd.read_csv("/data/training_items_sctt.csv")
-dfVal = pd.read_csv("/data/validation_items_sctt.csv")
-dfTest = pd.read_csv("/data/test_items_sctt.csv")
+dfTrain = pd.read_csv("C:\\Users\\samgr\\PycharmProjects\\ca-polyencoder-official\\data\\training_items_sctt.csv")
+dfVal = pd.read_csv("C:\\Users\\samgr\\PycharmProjects\\ca-polyencoder-official\\data\\validation_items_sctt.csv")
+dfTest = pd.read_csv("C:\\Users\\samgr\\PycharmProjects\\ca-polyencoder-official\\data\\test_items_sctt.csv")
 
 # Keep only relevant columns
 dfTrain = dfTrain[["text", "response", "label"]]
@@ -13,6 +13,6 @@ dfVal = dfVal[["text", "response", "label"]]
 dfTest = dfTest[["text", "response", "label"]]
 
 # Save filtered files
-dfTrain.to_csv("/data/train.csv")
-dfVal.to_csv("/data/val.csv")
-dfTest.to_csv("/data/test.csv")
+dfTrain.to_csv("C:\\Users\\samgr\\PycharmProjects\\ca-polyencoder-official\\data\\train.csv")
+dfVal.to_csv("C:\\Users\\samgr\\PycharmProjects\\ca-polyencoder-official\\data\\val.csv")
+dfTest.to_csv("C:\\Users\\samgr\\PycharmProjects\\ca-polyencoder-official\\data\\test.csv")
